edf6b11c492282eb22bbb939781035681287722a
мetlitsky <94578037+usernamemaybeunavailable@users.noreply.github.com>
2021-12-11 13:41:22 -0500

    fixes & updates to code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -31,38 +31,23 @@ import static frc.robot.Constants.*;
  */
 public class Robot extends TimedRobot {
     // defines the motors and runs initalization code
-private CANSparkMax = m_leftPrimary = initController(CAN.driveLeftPrimary);
-private CANSparkMax = m_rightPrimary = initController(CAN.driveRightPrimary);
-private CANSparkMax = m_leftFollower = initController(CAN.driveLeftFollower);
-private CANSparkMax = m_rightFollower = initController(CAN.driveRightFollower);
+private CANSparkMax m_leftPrimary = initController(CAN.driveLeftPrimary);
+private CANSparkMax m_rightPrimary = initController(CAN.driveRightPrimary);
+private CANSparkMax m_leftFollower = initController(CAN.driveLeftFollower);
+private CANSparkMax m_rightFollower = initController(CAN.driveRightFollower);
 
-private Timer m_timer =  new timer();
-
-private CANSparkMax initController(int port) { 
-    CANSparkmax controler = new CANSparkMax(port, MotorType.kBrushless);
-    controller.restoreFactoryDefaults();
-    controller.setIdleMode(IdleMode.kBrake);
-    return controller;
-}
-@Override
-public void robotInit() { 
-    m_leftFollower.follow(m_leftPrimary);
-    m_rightFollower.follow(m_rightPrimary);
-}
-
-
-
-    //  creates Differential Drive object
+ //  creates Differential Drive object
+private DifferentialDrive m_drive = new DifferentialDrive(m_leftPrimary, m_rightPrimary);
 
     // timer
-
+private Timer m_timer = new Timer ();
     // SparkMax setup
     private CANSparkMax initController(int port) {
         CANSparkMax controller = new CANSparkMax(port, MotorType.kBrushless);
         controller.restoreFactoryDefaults();
         controller.setIdleMode(IdleMode.kBrake);
         return controller;
-    }
+}
 
     /**
      * This function is run when the robot is first started up and should be used
@@ -70,6 +55,8 @@ public void robotInit() {
      */
     @Override
     public void robotInit() {
+        m.leftfollower.follow(m_leftPrimary);
+        m_rightFollower.follow(m_rightPrimary);
     }
 
     /**
@@ -102,6 +89,8 @@ public void robotInit() {
      */
     @Override
     public void autonomousInit() {
+        m_timer.reset();
+        m_timer.start();
     }
 
     /**
@@ -109,6 +98,12 @@ public void robotInit() {
      */
     @Override
     public void autonomousPeriodic() {
+        if (m_timer.get() < 2) {
+            m_drive.arcadeDrive(0.5,0);
+        }
+        else {
+            m_drive.stopMotor();
+        }
     }
 
     @Override
