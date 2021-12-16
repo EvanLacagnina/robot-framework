4203d62c63ffb42ed26e0fd959dba7056a868b00
Jason Hsu <94578139+JH485@users.noreply.github.com>
2021-12-15 19:24:45 -0500

    Adding Spinner Constants (#10)
    * added WPILib license
    
    * added spinner constants
    
    * remove spinner up down
    
    Co-authored-by: Michael Yu <64374317+itsmichaelyu@users.noreply.github.com>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,5 +6,7 @@ public class Constants {
         public static final int driveLeftFollower = 12;
         public static final int driveRightPrimary = 13;
         public static final int driveRightFollower = 14;
+
+        public static final int kSpinnerLeftRight = 41;
     }
 }
